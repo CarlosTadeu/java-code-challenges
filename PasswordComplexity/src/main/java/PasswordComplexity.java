3d7c7ab381f194dfb99b7100fdeedf4302be0086
Carlos Tadeu <carlos.santos@opus-software.com.br>
2021-10-21 20:09:41 -0300

    add PasswordComplexity

---
+++ PasswordComplexity/src/main/java/PasswordComplexity.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PasswordComplexity {
+
+    public static boolean isPasswordComplex(String password) {
+        if (password == null || password.length() < 6) {
+            return false;
+        }
+
+        boolean isDigit = false;
+        boolean isUpperCase = false;
+        boolean isLowerCase = false;
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isDigit(password.charAt(i)))
+                isDigit = true;
+            if (Character.isUpperCase(password.charAt(i)))
+                isUpperCase = true;
+            if (Character.isLowerCase(password.charAt(i)))
+                isLowerCase = true;
+        }
+        return isDigit && isUpperCase && isLowerCase;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a password: ");
+        String userInput = scanner.nextLine();
+        System.out.println("Is the password complex? " +
+                isPasswordComplex(userInput));
+    }
+}
